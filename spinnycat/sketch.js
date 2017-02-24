0ba7f00a65ef10beec53052c1d8033fb829e1162
Taurin Barrera <taurindb@gmail.com>
2017-02-23 21:49:43 -0800

    Added spinnycat sketch

---
+++ spinnycat/sketch.js
@@ -0,0 +1,19 @@
+var kitty;
+var kittyImg;
+function preload() {
+  kittyImg = loadImage('assets/cat.png');
+}
+function setup() {
+  createCanvas(windowHeight, windowWidth);
+  kitty = createSprite(width/2, height/2);
+  kitty.addImage(kittyImg);
+}
+function draw() {
+  kitty.position.x = mouseX;
+  kitty.position.y = mouseY;
+  if (mouseIsPressed) {
+    kitty.rotation += 2;
+  }
+  drawSprites();
+}
+
