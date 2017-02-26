c344cb8db4f344f3a8091ff1e68314ff35910b6d
Taurin Barrera <taurindb@gmail.com>
2017-02-26 09:49:55 -0800

    the gib is back

--- spinnycat/sketch.js
+++ spinnycat/sketch.js
@@ -24,6 +24,26 @@ function draw() {
     tint(200, 153, x, 126);
   }
   drawSprites();
+
+a = Drums('x*o*x*o-')
+a.pitch = Mouse.y
+
+b = FM({ attack: ms(1) })
+b.index = a.out
+b.cmRatio = Mouse.x
+
+b.fx.add(
+  Delay({
+    time:     Mouse.x,
+    feedback: Mouse.y
+  })
+)
+
+b.note.seq( 
+  ['c2','c2','c2','c3','c4'].random(),
+  [1/4,1/8,1/16].random(1/16,2) 
+)
+
 }
 
 function windowResized() {
